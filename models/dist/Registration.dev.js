639d02a80a2d0ffc7599b951be671a9be7b859bc
Sokos6 <will.sokolowski@gmail.com>
2020-08-22 18:09:55 -0500

    Registration

---
+++ models/dist/Registration.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var mongoose = require('mongoose');
+
+var registrationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true
+  }
+});
+module.exports = mongoose.model('Registration', registrationSchema);
